cc5342979a1cf8e82c6c25407da6bc7b9e5ccdd2
João Paulo Busche <joaopaulo.busche@gmail.com>
2022-11-02 17:18:40 -0300

    Change Jump Logic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -11,18 +11,30 @@ public class Player : Singleton<Player> {
     [Header("Animations")]
     [SerializeField] Animator myAnim;
     [SerializeField] string runTrigger = "Run";
+    [Header("Jump")]
+    [SerializeField] float jumpSpeed;
+    [SerializeField] KeyCode jumpKey;
 
     CharacterController _myChar;
-    float _currentSpeed;
+    float _currentSpeed, _verticalSpeed;
 
     void Start() {
         _myChar = GetComponent<CharacterController>();
+        _verticalSpeed = 0f;
     }
 
     void Update() {
         _currentSpeed = Input.GetAxis("Vertical");
+        _verticalSpeed -= gravity * Time.deltaTime;
+        HandleJump();
         HandleMoviments();
         HandleAnimation();
+    }
+
+    void HandleJump() {
+        if (_myChar.isGrounded && Input.GetKeyDown(jumpKey)) {
+            _verticalSpeed = jumpSpeed;
+        }
     }
 
     void HandleAnimation() {
@@ -33,7 +45,7 @@ public class Player : Singleton<Player> {
         Vector3 rotation = new Vector3(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
         transform.Rotate(rotation);
         Vector3 frontSpeed = transform.forward * _currentSpeed * speed;
-        frontSpeed.y = Time.deltaTime * (-gravity);
+        frontSpeed.y = _verticalSpeed;
         _myChar.Move(frontSpeed * Time.deltaTime);
     }
 
