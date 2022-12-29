02f0ff8138c4be39739955a15200d3a62a6848fa
João Paulo Busche <joaopaulo.busche@gmail.com>
2022-12-29 15:02:35 -0300

    Make Enemy Looks at Player

---
+++ Assets/Scripts/Enemy/EnemyLooks.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLooks : EnemyBase {
+
+    [Header("Look At")]
+    [SerializeField] Transform lookAt;
+
+    void Update() {
+        transform.LookAt(lookAt.position);
+    }
+
+}
