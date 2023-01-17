e8e3336c5cee955e9e508a5193c4319009468b1a
João Paulo Busche <joaopaulo.busche@gmail.com>
2023-01-17 19:26:59 -0300

    Create Cloth Manager

---
+++ Assets/Scripts/Managers/ClothManager.cs
@@ -0,0 +1,28 @@
+using System.Security.AccessControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothManager : Singleton<ClothManager> {
+  
+    [SerializeField] List<ClothSetup> clothSetups;
+
+    public ClothSetup GetSetupByType(ClothType type) {
+        return clothSetups.Find(i => i.clothType == type);
+    }
+
+}
+
+public enum ClothType {
+    BASIC_V2,
+    SPEED,
+    DEFENSE
+}
+
+[System.Serializable]
+public class ClothSetup {
+
+    public ClothType clothType;
+    public Texture clothTexture;
+    public Color visorColor;
+}
